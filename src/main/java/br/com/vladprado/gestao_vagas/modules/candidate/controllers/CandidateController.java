62c70eacf2df2f8e09981afb9030f59f5e4c540d
vladprado <vladprado.br@gmail.com>
2023-11-26 16:16:49 -0300

    Criacao da Service para Candidate e nova Entidade Company

--- src/main/java/br/com/vladprado/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/vladprado/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,24 +1,32 @@
 package br.com.vladprado.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.vladprado.gestao_vagas.exceptions.UserFoundException;
 import br.com.vladprado.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.vladprado.gestao_vagas.modules.candidate.UseCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
-    
+
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUseCase createCandidateUseCase;
+
 
     @PostMapping("/")
-    public @Valid CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        return this.candidateRepository.save(candidateEntity);
-        
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try { 
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
--- src/main/java/br/com/vladprado/gestao_vagas/modules/candidate/controllers/CandidateRepository.java
+++ src/main/java/br/com/vladprado/gestao_vagas/modules/candidate/controllers/CandidateRepository.java
@@ -1,5 +1,6 @@
 package br.com.vladprado.gestao_vagas.modules.candidate.controllers;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.vladprado.gestao_vagas.modules.candidate.CandidateEntity;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
-    
+    Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
 }
